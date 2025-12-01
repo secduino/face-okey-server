ac0a104c0b6f512bebccc531f375db57b2a9ce2d
secduino <secosecduino@gmail.com>
2025-12-02 01:34:06 +0300

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 
 const gameSocket = require("./sockets/game_socket");
+const vipSocket = require("./sockets/vip_socket"); // VIP Sockets
 
+// ------------------------------------------------
+// APP
+// ------------------------------------------------
 const app = express();
 app.use(cors());
 
@@ -12,8 +16,19 @@ app.get("/", (req, res) => {
   res.send("FACE Okey Server Running");
 });
 
+// ------------------------------------------------
+// HTTP SERVER
+// ------------------------------------------------
 const server = http.createServer(app);
 
+// ------------------------------------------------
+// GLOBAL ROOM LIST (VIP Rooms)
+// ------------------------------------------------
+const rooms = []; // VIP odalar burada tutulacak
+
+// ------------------------------------------------
+// SOCKET.IO
+// ------------------------------------------------
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -21,17 +36,26 @@ const io = new Server(server, {
   }
 });
 
-// SOCKET BAŞLAT
+// ------------------------------------------------
+// SOCKET CONNECTION
+// ------------------------------------------------
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
+  // Game Logic
   gameSocket(io, socket);
+
+  // VIP Room Logic
+  vipSocket(io, socket, rooms);
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
   });
 });
 
+// ------------------------------------------------
+// SERVER LISTEN
+// ------------------------------------------------
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Server running on port:", PORT);
