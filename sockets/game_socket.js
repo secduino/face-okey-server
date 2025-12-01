c6b0ac6984148032a42d34ed2dab289976ee6f11
secduino <secosecduino@gmail.com>
2025-12-02 00:48:00 +0300

    game_socket.js

--- sockets/game_socket.js
+++ sockets/game_socket.js
@@ -1,39 +1,82 @@
-const roomController = require("../controllers/room_controller");
-
 module.exports = (io, socket) => {
-  
-  // ODA OLUŞTUR
-  socket.on("createRoom", (data) => {
-    const roomId = roomController.createRoom();
+  // Normal odalar ile karışmasın diye ayrı VIP room listesi
+  const vipRooms = {};
+
+  function generateRoomId() {
+    return Math.random().toString(36).substr(2, 6).toUpperCase();
+  }
+
+  // ----------------------------------------------------
+  // VIP ODA OLUŞTUR
+  // ----------------------------------------------------
+  socket.on("vip:create_room", (data) => {
+    console.log("VIP Oda oluşturma isteği:", data);
+
+    const roomId = generateRoomId();
+
+    const room = {
+      id: roomId,
+      name: data.name,
+      bet: data.bet,
+      ownerId: data.ownerId,
+      players: [
+        {
+          id: data.ownerId,
+          socketId: socket.id,
+        }
+      ]
+    };
+
+    vipRooms[roomId] = room;
+
     socket.join(roomId);
 
-    socket.emit("roomCreated", { roomId });
-    console.log("Room created:", roomId);
+    console.log("VIP Oda oluşturuldu:", room);
+
+    // Flutter tarafına oda bilgisi dön
+    socket.emit("vip:room_created", room);
   });
 
-  // ODAYA KATIL
-  socket.on("joinRoom", (data) => {
-    const ok = roomController.joinRoom(data.roomId);
+  // ----------------------------------------------------
+  // VIP ODA KATIL
+  // ----------------------------------------------------
+  socket.on("vip:join_room", (data) => {
+    const { roomId, userId } = data;
 
-    if (!ok) {
-      socket.emit("joinError", { message: "Room is full or not exist" });
+    if (!vipRooms[roomId]) {
+      socket.emit("vip:join_error", { message: "Oda bulunamadı" });
       return;
     }
 
-    socket.join(data.roomId);
-    socket.emit("joinedRoom", { roomId: data.roomId });
-
-    socket.to(data.roomId).emit("playerJoined", { id: socket.id });
-
-    console.log("Player joined room:", data.roomId);
-  });
-
-  // MESAJ
-  socket.on("sendMessage", (data) => {
-    io.to(data.roomId).emit("receiveMessage", {
-      id: socket.id,
-      message: data.message
+    vipRooms[roomId].players.push({
+      id: userId,
+      socketId: socket.id,
     });
+
+    socket.join(roomId);
+
+    io.to(roomId).emit("vip:room_updated", vipRooms[roomId]);
   });
 
+  // ----------------------------------------------------
+  // VIP ODADAN AYRIL
+  // ----------------------------------------------------
+  socket.on("vip:leave_room", (data) => {
+    const { roomId, userId } = data;
+
+    if (!vipRooms[roomId]) return;
+
+    vipRooms[roomId].players = vipRooms[roomId].players.filter(
+      (p) => p.id !== userId
+    );
+
+    socket.leave(roomId);
+
+    // Oda boşaldıysa sil
+    if (vipRooms[roomId].players.length === 0) {
+      delete vipRooms[roomId];
+    } else {
+      io.to(roomId).emit("vip:room_updated", vipRooms[roomId]);
+    }
+  });
 };
